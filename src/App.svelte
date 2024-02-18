eb0c0c66e21d0c90daad90217a7c97f5dea939e9
hotsixisbest <galactagun12@gmail.com>
2024-02-19 00:03:17 +0900

    header 로고 추가

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import {Router, Route} from 'svelte-routing';
+  import Header from './lib/components/header.svelte';
+  //import {default as Index} from './lib/routes/page.svelte';
+
+  export let url = "";
+</script>
+
+<Header/>
+<main>
+  <!--
+  <Router {url}>
+      <Route path="/" component={Index}/>
+  </Router>
+  -->
+</main>
