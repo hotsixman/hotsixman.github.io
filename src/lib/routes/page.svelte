e9b1ac9d006f7c1dd3d66afd57cc9efb6bab5138
hotsixisbest <galactagun12@gmail.com>
2024-02-19 10:11:47 +0900

    timeline 등장 애니메이션 수정

--- src/lib/routes/page.svelte
+++ src/lib/routes/page.svelte
@@ -7,4 +7,7 @@ import Timeline from "./timeline.svelte";
     <TimelineRow date="2023.4 ~ 2024.2">
         taikowiki.com 개발
     </TimelineRow>
+    <TimelineRow date="2023.11 ~">
+        node-hiroba 개발
+    </TimelineRow>
 </Timeline>
