4487ae1461b8c8e36b461fcd6ac45237bb9114ec
hotsixisbest <galactagun12@gmail.com>
2024-02-26 02:18:53 +0900

    projects 경로 추가

--- src/lib/routes/projects/page.svelte
+++ src/lib/routes/projects/page.svelte
@@ -1,1 +1,11 @@
-asdasd
+<script>
+    import ProjectContainer from "./project-container.svelte";
+    import Project from "./project.svelte";
+
+    import taikowiki from '../../../assets/projects/taikowiki.png';
+
+</script>
+<ProjectContainer>
+    <Project title="태고의 달인 한국어 위키" src={taikowiki} href="/projects/taikowiki"/>
+    <Project title="node-hiroba"/>
+</ProjectContainer>
---
+++ src/lib/routes/projects/project.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    export let title:string;
+    export let src:string = '';
+    export let href:string = '';
+</script>
+
+<a class="project" href={href?`/#${href}`:null}>
+    <div class="title">
+        {title}
+    </div>
+    <div class="img-container" style={src? `background-image:url('${src}')` : ``}/>
+</a>
+
+<style>
+    .project{
+        box-sizing: border-box;
+
+        width:300px;
+        height:260px;
+
+        border:5px solid white;
+        border-radius: 10px;
+
+        cursor: pointer;
+
+        color:white;
+        text-decoration: none;
+    }
+
+    .title{
+        width:100%;
+        height:40px;
+
+        display:flex;
+        justify-content: center;
+        align-items: center;
+
+        font-size: 26px;
+        font-weight: bold;
+    }
+
+    .img-container{
+        width:100%;
+        height:calc(100% - 40px);
+
+        background-size:contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .project:hover{
+        border: 5px solid #00f9ff;
+        box-shadow: 0 0 20px #57fcff;
+    }
+</style>
