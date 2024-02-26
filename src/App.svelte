8f74390c1272c00cced408739a619258892e7501
hotsixisbest <galactagun12@gmail.com>
2024-02-26 17:29:45 +0900

    isMobile 스토어 추가

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,19 @@
   import Header from './lib/components/header/header.svelte';
   import {default as Index} from './lib/routes/page.svelte';
   import {default as Projects} from './lib/routes/projects/page.svelte';
+    import { writable } from 'svelte/store';
+    import { onMount, setContext } from 'svelte';
 
   const routes = {
     '/': Index,
     '/projects': Projects
   }
+
+  let isMobile = writable(window.innerWidth < 768);
+  window.addEventListener('resize', () => {
+    $isMobile = window.innerWidth < 768;
+  });
+  setContext('isMobile', isMobile);
 </script>
 
 <Header/>
